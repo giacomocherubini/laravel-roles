5decb6d1fa2314b4dedd11457fcbdc095f6e0622
giacomocherubini <giacomo.cherubini1@gmail.com>
2019-07-24 14:12:52 +0200

    index

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="container">
+    <h1>Tutti gli occhiali</h1>
+    <table>
+      <thead>
+        <th>ID</th>
+        <th>Marca</th>
+        <th>Description</th>
+        <th>Price</th>
+
+      </thead>
+    </table>
+
+  </div>
+
+@endsection
